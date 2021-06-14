567d143f8debd87326430f574534539c32cbfbd9
verils <verils@qq.com>
2021-06-14 19:00:55 +0800

    Optimize test

--- src/test/java/com/github/verils/gotemplate/lex/LexerTest.java
+++ src/test/java/com/github/verils/gotemplate/lex/LexerTest.java
@@ -10,10 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LexerTest {
 
-    private static final Item EOF_ITEM = new Item(ItemType.EOF, "", 0);
-    private static final Item LEFT_DELIM_ITEM = new Item(ItemType.LEFT_DELIM, "{{", 0);
-    private static final Item RIGHT_DELIM_ITEM = new Item(ItemType.RIGHT_DELIM, "}}", 0);
-    public static final Item SPACE_ITEM = new Item(ItemType.SPACE, " ", 0);
+    private static final Item EOF_ITEM = makeItem(ItemType.EOF, "");
+    private static final Item LEFT_DELIM_ITEM = makeItem(ItemType.LEFT_DELIM, "{{");
+    private static final Item RIGHT_DELIM_ITEM = makeItem(ItemType.RIGHT_DELIM, "}}");
+    public static final Item SPACE_ITEM = makeItem(ItemType.SPACE, " ");
 
     @BeforeEach
     void setUp() {
@@ -28,28 +28,28 @@ class LexerTest {
         }
 
         var tests = new Test[]{
-                new Test("", new Item[]{new Item(ItemType.EOF, "", 0)}),
+                new Test("", new Item[]{makeItem(ItemType.EOF, "")}),
                 new Test(" \t\n", new Item[]{
-                        new Item(ItemType.TEXT, " \t\n", 0),
+                        makeItem(ItemType.TEXT, " \t\n"),
                         EOF_ITEM
                 }),
                 new Test("now is the time", new Item[]{
-                        new Item(ItemType.TEXT, "now is the time", 0),
+                        makeItem(ItemType.TEXT, "now is the time"),
                         EOF_ITEM
                 }),
                 new Test("{{,@% }}", new Item[]{
                         LEFT_DELIM_ITEM,
-                        new Item(ItemType.CHAR, ",", 0),
-                        new Item(ItemType.CHAR, "@", 0),
-                        new Item(ItemType.CHAR, "%", 0),
+                        makeItem(ItemType.CHAR, ","),
+                        makeItem(ItemType.CHAR, "@"),
+                        makeItem(ItemType.CHAR, "%"),
                         SPACE_ITEM,
                         RIGHT_DELIM_ITEM,
                         EOF_ITEM
                 }),
                 new Test("hello-{{/* this is a comment */}}-world", new Item[]{
-                        new Item(ItemType.TEXT, "hello-", 0),
-                        new Item(ItemType.COMMENT, "/* this is a comment */", 0),
-                        new Item(ItemType.TEXT, "-world", 0),
+                        makeItem(ItemType.TEXT, "hello-"),
+                        makeItem(ItemType.COMMENT, "/* this is a comment */"),
+                        makeItem(ItemType.TEXT, "-world"),
                         EOF_ITEM
                 })
         };
@@ -62,5 +62,9 @@ class LexerTest {
                 assertEquals(item.getType(), tgt.getType(), "Input: '" + test.input + "'");
             }
         }
+    }
+
+    private static Item makeItem(ItemType type, String value) {
+        return new Item(type, value, 0);
     }
 }
