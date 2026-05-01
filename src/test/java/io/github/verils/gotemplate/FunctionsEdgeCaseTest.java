d3d108217b58dba44a98a7654889ed64f3b11b2f
Yes, Bob <verils@qq.com>
2026-05-01 22:21:05 +0800

    test(functions): add comprehensive edge case tests for Go template functions
    - Replace Integer.valueOf() with primitive int literals in test data
    - Remove redundant slice with end beyond length test case
    - Add null value comparison tests for eq, ne, lt, le, gt, ge functions
    - Add boolean logic tests for and, or, not with various falsy values
    - Add length function tests with empty collections (string, array, list, map)
    - Add boundary condition tests for index and slice operations
    - Add function call tests with multiple arguments
    - Add escaping function tests for html and js with special characters
    - Add deepEqual tests with null and different typed values
    - Add type checking tests for typeof and kindOf functions
    - Add truthiness tests for numbers and strings in conditional contexts
    - Add chained comparison operator tests for lt, le, gt, ge
    - Add integer conversion tests from null and floating point values
    
    Signed-off-by: Yes, Bob <verils@qq.com>

--- src/test/java/io/github/verils/gotemplate/FunctionsEdgeCaseTest.java
+++ src/test/java/io/github/verils/gotemplate/FunctionsEdgeCaseTest.java
@@ -219,7 +219,7 @@ class FunctionsEdgeCaseTest {
             template.parse("{{len .Value}}");
             Writer writer = new StringWriter();
             Map<String, Object> data = new HashMap<>();
-            data.put("Value", Integer.valueOf(42));
+            data.put("Value", 42);
             template.execute(writer, data);
         });
     }
@@ -231,7 +231,7 @@ class FunctionsEdgeCaseTest {
             template.parse("{{index .Value 0}}");
             Writer writer = new StringWriter();
             Map<String, Object> data = new HashMap<>();
-            data.put("Value", Integer.valueOf(42));
+            data.put("Value", 42);
             template.execute(writer, data);
         });
     }
@@ -243,7 +243,7 @@ class FunctionsEdgeCaseTest {
             template.parse("{{slice .Value 0 1}}");
             Writer writer = new StringWriter();
             Map<String, Object> data = new HashMap<>();
-            data.put("Value", Integer.valueOf(42));
+            data.put("Value", 42);
             template.execute(writer, data);
         });
     }
@@ -269,15 +269,6 @@ class FunctionsEdgeCaseTest {
         assertNotNull(writer.toString());
     }
 
-    @Test
-    void testSliceWithEndBeyondLength() throws IOException, TemplateException {
-        Template template = new Template("test");
-        template.parse("{{slice \"hello\" 0 100}}");
-        
-        Writer writer = new StringWriter();
-        template.execute(writer, null);
-        assertEquals("hello", writer.toString());
-    }
 
     @Test
     void testSliceWithStartGreaterThanOrEqualEnd() throws IOException, TemplateException {
@@ -406,5 +397,488 @@ class FunctionsEdgeCaseTest {
         data.put("Items", new String[]{"a", "b", "c"});
         template.execute(writer, data);
         assertEquals("a", writer.toString()); // Invalid index defaults to 0
+    }
+
+    @Test
+    void testEqWithNullValues() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if eq .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", null);
+        data.put("B", null);
+        template.execute(writer, data);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testEqWithOneNullValue() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if eq .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", "value");
+        data.put("B", null);
+        template.execute(writer, data);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testNeWithNullValues() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if ne .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", null);
+        data.put("B", null);
+        template.execute(writer, data);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testLtWithEqualValues() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if lt 2 2}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testLeWithLessThan() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if le 1 2}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testGtWithEqualValues() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if gt 2 2}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testGeWithGreaterThan() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if ge 3 2}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testGeWithLessThan() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if ge 1 2}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testAndWithAllFalseValues() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{and false false false}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("false", writer.toString());
+    }
+
+    @Test
+    void testOrWithAllFalseValues() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{or 0 \"\" false}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("false", writer.toString());
+    }
+
+    @Test
+    void testNotWithNull() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if not .Value}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", null);
+        template.execute(writer, data);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testNotWithEmptyString() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if not .Value}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", "");
+        template.execute(writer, data);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testNotWithZero() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if not .Value}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", 0);
+        template.execute(writer, data);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testLenWithEmptyString() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{len .Value}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", "");
+        template.execute(writer, data);
+        assertEquals("0", writer.toString());
+    }
+
+    @Test
+    void testLenWithEmptyArray() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{len .Value}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", new String[]{});
+        template.execute(writer, data);
+        assertEquals("0", writer.toString());
+    }
+
+    @Test
+    void testLenWithEmptyList() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{len .Value}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", new ArrayList<>());
+        template.execute(writer, data);
+        assertEquals("0", writer.toString());
+    }
+
+    @Test
+    void testLenWithEmptyMap() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{len .Value}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", new HashMap<>());
+        template.execute(writer, data);
+        assertEquals("0", writer.toString());
+    }
+
+    @Test
+    void testIndexWithStringOutOfBounds() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{index .Text 100}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Text", "hi");
+        template.execute(writer, data);
+        assertEquals("", writer.toString());
+    }
+
+    @Test
+    void testIndexWithNegativeArrayIndex() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{index .Items -1}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Items", new String[]{"a", "b", "c"});
+        template.execute(writer, data);
+        // Negative index behavior may vary, just ensure it doesn't crash
+        assertNotNull(writer.toString());
+    }
+
+    @Test
+    void testSliceWithNegativeStart() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{slice .Text -5 3}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Text", "hello");
+        template.execute(writer, data);
+        // Negative start is clamped to 0, so this becomes slice 0 3
+        assertNotNull(writer.toString());
+    }
+
+    @Test
+    void testSliceWithEndBeyondLength() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{slice .Text 0 100}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Text", "hi");
+        template.execute(writer, data);
+        assertEquals("hi", writer.toString());
+    }
+
+    @Test
+    void testSliceWithArrayEmptyResult() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{slice .Items 3 2}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Items", new String[]{"a", "b", "c"});
+        template.execute(writer, data);
+        assertNotNull(writer.toString());
+    }
+
+    @Test
+    void testCallFunctionWithArgs() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{call .Func \"arg1\" \"arg2\"}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Func", (Function) args -> args[0] + "-" + args[1]);
+        template.execute(writer, data);
+        assertEquals("arg1-arg2", writer.toString());
+    }
+
+    @Test
+    void testHtmlWithAmpersand() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{html .Text}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Text", "a&b");
+        template.execute(writer, data);
+        assertEquals("a&amp;b", writer.toString());
+    }
+
+    @Test
+    void testJsWithUnicodeChar() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{js .Text}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Text", "中文");
+        template.execute(writer, data);
+        // Unicode chars should be escaped
+        assertTrue(writer.toString().contains("\\u"));
+    }
+
+    @Test
+    void testJsWithControlChars() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{js .Text}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Text", "line1\r\nline2\ttab");
+        template.execute(writer, data);
+        assertTrue(writer.toString().contains("\\r") || writer.toString().contains("\r"));
+        assertTrue(writer.toString().contains("\\n") || writer.toString().contains("\n"));
+        assertTrue(writer.toString().contains("\\t") || writer.toString().contains("\t"));
+    }
+
+    @Test
+    void testDeepEqualWithNullArrays() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if deepEqual .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", null);
+        data.put("B", null);
+        template.execute(writer, data);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testDeepEqualWithOneNullArray() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if deepEqual .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", new int[]{1, 2});
+        data.put("B", null);
+        template.execute(writer, data);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testDeepEqualWithDifferentTypes() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if deepEqual .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", "string");
+        data.put("B", 42);
+        template.execute(writer, data);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testDeepEqualWithNestedArrays() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if deepEqual .A .B}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("A", new Object[]{new int[]{1, 2}, new int[]{3, 4}});
+        data.put("B", new Object[]{new int[]{1, 2}, new int[]{3, 4}});
+        template.execute(writer, data);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testTypeofWithArray() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{typeof .Value}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", new int[]{1, 2, 3});
+        template.execute(writer, data);
+        assertTrue(writer.toString().contains("int[]") || writer.toString().contains("[I"));
+    }
+
+    @Test
+    void testKindOfWithStruct() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{kindOf .Value}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", new Recipient("Test", "gift", true));
+        template.execute(writer, data);
+        assertEquals("struct", writer.toString());
+    }
+
+    @Test
+    void testIsTruthyWithNonZeroNumber() throws IOException, TemplateException {
+        // Test that non-zero numbers are truthy in conditional context
+        Template template = new Template("test");
+        template.parse("{{if ne .Value 0}}truthy{{else}}falsy{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", 42);
+        template.execute(writer, data);
+        assertEquals("truthy", writer.toString());
+    }
+
+    @Test
+    void testIsTruthyWithNonEmptyString() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if .Value}}truthy{{else}}falsy{{end}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Value", "hello");
+        template.execute(writer, data);
+        assertEquals("truthy", writer.toString());
+    }
+
+    @Test
+    void testCompareWithChainedLt() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if lt 1 2 3 4}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testCompareWithChainedLtFailing() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if lt 1 3 2 4}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("no", writer.toString());
+    }
+
+    @Test
+    void testCompareWithChainedLe() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if le 1 2 2 3}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testCompareWithChainedGt() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if gt 4 3 2 1}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testCompareWithChainedGe() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{if ge 4 3 3 2}}yes{{else}}no{{end}}");
+        
+        Writer writer = new StringWriter();
+        template.execute(writer, null);
+        assertEquals("yes", writer.toString());
+    }
+
+    @Test
+    void testToIntWithNull() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{index .Items .NullIndex}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Items", new String[]{"a", "b", "c"});
+        data.put("NullIndex", null);
+        template.execute(writer, data);
+        assertEquals("a", writer.toString()); // null converts to 0
+    }
+
+    @Test
+    void testToIntWithDouble() throws IOException, TemplateException {
+        Template template = new Template("test");
+        template.parse("{{index .Items .FloatIndex}}");
+        
+        Writer writer = new StringWriter();
+        Map<String, Object> data = new HashMap<>();
+        data.put("Items", new String[]{"a", "b", "c"});
+        data.put("FloatIndex", 1.9);
+        template.execute(writer, data);
+        assertEquals("b", writer.toString()); // 1.9 converts to 1
     }
 }
