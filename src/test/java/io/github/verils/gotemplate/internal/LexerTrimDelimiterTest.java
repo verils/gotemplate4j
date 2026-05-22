a80f2ed20510425e943cf4d3c426504394117aad
Yes, Bob <verils@qq.com>
2026-05-22 23:25:20 +0800

    test(lexer): remove lexer test class
    - Remove the entire LexerTest test class file
    - Delete all unit test cases related to lexer functionality
    - Remove tests for empty strings, whitespace characters, single-line text, etc.
    - Delete tests for various tokens such as comments, action special characters, numbers, strings, etc.
    - Remove tests for complex syntax including variables, pipelines, declarations, bracket fields, etc.
    - Delete test cases for various edge cases and error handling
    - Remove test methods for custom delimiters
    
    Signed-off-by: Yes, Bob <verils@qq.com>

--- src/test/java/io/github/verils/gotemplate/internal/LexerTrimDelimiterTest.java
+++ src/test/java/io/github/verils/gotemplate/internal/LexerTrimDelimiterTest.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 import static io.github.verils.gotemplate.internal.LexerTestSupport.assertDefaultTokens;
 import static io.github.verils.gotemplate.internal.LexerTestSupport.assertDelimiterTokens;
 import static io.github.verils.gotemplate.internal.LexerTestSupport.assertTokens;
+import static io.github.verils.gotemplate.internal.LexerTestSupport.defaultCase;
 import static io.github.verils.gotemplate.internal.LexerTestSupport.delimiterCase;
 import static io.github.verils.gotemplate.internal.LexerTestSupport.lexCustom;
 import static io.github.verils.gotemplate.internal.LexerTestSupport.token;
@@ -34,6 +35,133 @@ class LexerTrimDelimiterTest {
                 token(TokenType.COMMENT, "/* hello */", 11, 1, 0),
                 token(TokenType.TEXT, "-world", 27, 1, 0),
                 token(TokenType.EOF, "", 33, 1, 0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("leadingWhitespaceTrimMarkerCases")
+    void trimMarkerRemovesLeadingWhitespace(String input, Token[] expectedTokens) {
+        assertDefaultTokens(input, expectedTokens);
+    }
+
+    private static Stream<Arguments> leadingWhitespaceTrimMarkerCases() {
+        return Stream.of(
+                defaultCase("{{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 0, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 4, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 5, 1, 0),
+                        token(TokenType.EOF, "", 7, 1, 0)),
+                defaultCase("  {{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 2, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 6, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 7, 1, 0),
+                        token(TokenType.EOF, "", 9, 1, 0)),
+                defaultCase("\t{{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 1, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 5, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 6, 1, 0),
+                        token(TokenType.EOF, "", 8, 1, 0)),
+                defaultCase("\n{{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 1, 2, 0),
+                        token(TokenType.IDENTIFIER, "x", 5, 2, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 6, 2, 0),
+                        token(TokenType.EOF, "", 8, 2, 0)),
+                defaultCase("\r{{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 1, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 5, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 6, 1, 0),
+                        token(TokenType.EOF, "", 8, 1, 0)),
+                defaultCase(" \t\r\n{{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 4, 2, 0),
+                        token(TokenType.IDENTIFIER, "x", 8, 2, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 9, 2, 0),
+                        token(TokenType.EOF, "", 11, 2, 0)),
+                defaultCase("      {{- x}}",
+                        token(TokenType.LEFT_DELIM, "{{", 6, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 10, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 11, 1, 0),
+                        token(TokenType.EOF, "", 13, 1, 0)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyTrimActionCases")
+    void trimMarkerSupportsEmptyActions(String input, Token[] expectedTokens) {
+        assertDefaultTokens(input, expectedTokens);
+    }
+
+    private static Stream<Arguments> emptyTrimActionCases() {
+        return Stream.of(
+                defaultCase("{{- }}",
+                        token(TokenType.LEFT_DELIM, "{{", 0, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 4, 1, 0),
+                        token(TokenType.EOF, "", 6, 1, 0)),
+                defaultCase("  {{- }}",
+                        token(TokenType.LEFT_DELIM, "{{", 2, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 6, 1, 0),
+                        token(TokenType.EOF, "", 8, 1, 0)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("trimCommentCases")
+    void trimMarkerSupportsStandaloneComments(String input, Token[] expectedTokens) {
+        assertDefaultTokens(input, expectedTokens);
+    }
+
+    private static Stream<Arguments> trimCommentCases() {
+        return Stream.of(
+                defaultCase("{{- /* comment */ -}}",
+                        token(TokenType.COMMENT, "/* comment */", 4, 1, 0),
+                        token(TokenType.EOF, "", 21, 1, 0)),
+                defaultCase(" \t\n{{- /* comment */ -}}",
+                        token(TokenType.COMMENT, "/* comment */", 7, 2, 0),
+                        token(TokenType.EOF, "", 24, 2, 0)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("textBeforeTrimMarkerCases")
+    void trimMarkerTrimsWhitespaceAfterPreviousText(String input, Token[] expectedTokens) {
+        assertDefaultTokens(input, expectedTokens);
+    }
+
+    private static Stream<Arguments> textBeforeTrimMarkerCases() {
+        return Stream.of(
+                defaultCase("hello {{- x}}",
+                        token(TokenType.TEXT, "hello", 0, 1, 0),
+                        token(TokenType.LEFT_DELIM, "{{", 6, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 10, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 11, 1, 0),
+                        token(TokenType.EOF, "", 13, 1, 0)),
+                defaultCase("a {{- x}}",
+                        token(TokenType.TEXT, "a", 0, 1, 0),
+                        token(TokenType.LEFT_DELIM, "{{", 2, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 6, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 7, 1, 0),
+                        token(TokenType.EOF, "", 9, 1, 0)),
+                defaultCase("hello\n{{- x}}",
+                        token(TokenType.TEXT, "hello", 0, 1, 0),
+                        token(TokenType.LEFT_DELIM, "{{", 6, 2, 0),
+                        token(TokenType.IDENTIFIER, "x", 10, 2, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 11, 2, 0),
+                        token(TokenType.EOF, "", 13, 2, 0)),
+                defaultCase("0123{{hello}}  {{- x}}",
+                        token(TokenType.TEXT, "0123", 0, 1, 0),
+                        token(TokenType.LEFT_DELIM, "{{", 4, 1, 0),
+                        token(TokenType.IDENTIFIER, "hello", 6, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 11, 1, 0),
+                        token(TokenType.LEFT_DELIM, "{{", 15, 1, 0),
+                        token(TokenType.IDENTIFIER, "x", 19, 1, 0),
+                        token(TokenType.RIGHT_DELIM, "}}", 20, 1, 0),
+                        token(TokenType.EOF, "", 22, 1, 0)));
+    }
+
+    @Test
+    void trimMarkersOnBothSidesRemoveSurroundingWhitespace() {
+        assertDefaultTokens("hello {{- x -}} world",
+                token(TokenType.TEXT, "hello", 0, 1, 0),
+                token(TokenType.LEFT_DELIM, "{{", 6, 1, 0),
+                token(TokenType.IDENTIFIER, "x", 10, 1, 0),
+                token(TokenType.RIGHT_DELIM, "}}", 13, 1, 0),
+                token(TokenType.TEXT, "world", 16, 1, 0),
+                token(TokenType.EOF, "", 21, 1, 0));
     }
 
     @ParameterizedTest
